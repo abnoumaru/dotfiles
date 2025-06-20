6cb05bf5d1399942ca52003873eae048d61cb5fd
Takaaki ABE <abnoumaru@gmail.com>
2025-06-21 05:58:25 +0900

    neovim

--- neovim/init.lua
+++ neovim/init.lua
@@ -1,14 +1,24 @@
--- basic settings---------------------------
-vim.opt.encoding='utf8'
-vim.wo.number=true
-vim.o.ruler=true
-vim.wo.cursorline=true
-vim.wo.cursorcolumn=true
-vim.bo.expandtab=true
-vim.bo.tabstop=2
-vim.bo.shiftwidth=2
-vim.bo.autoindent=true
---End basic settings-----------------------
+-- Basic settings
+vim.opt.encoding = 'utf8'
+vim.wo.number = true
+vim.o.ruler = true
+vim.wo.cursorline = true
+vim.wo.cursorcolumn = true
+vim.bo.expandtab = true
+vim.bo.tabstop = 2
+vim.bo.shiftwidth = 2
+vim.bo.autoindent = true
+
+-- Additional helpful settings
+vim.opt.clipboard = "unnamedplus"  -- Use system clipboard
+vim.opt.ignorecase = true          -- Case insensitive search
+vim.opt.smartcase = true           -- Case sensitive if uppercase used
+vim.opt.hlsearch = true            -- Highlight search results
+vim.opt.incsearch = true           -- Incremental search
+vim.opt.scrolloff = 8              -- Keep 8 lines above/below cursor
+vim.opt.signcolumn = "yes"         -- Always show sign column
+
+-- Lazy.nvim setup
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -16,18 +26,304 @@ if not vim.loop.fs_stat(lazypath) then
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    "--branch=stable",
     lazypath,
   })
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup(plugins, opts)
+-- Plugin specifications
+local plugins = {
+  -- Cyberdream colorscheme
+  {
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("cyberdream").setup({
+        transparent = true,
+        italic_comments = true,
+        hide_fillchars = true,
+        borderless_telescope = true,
+        terminal_colors = true,
+      })
+      vim.cmd("colorscheme cyberdream")
+    end,
+  },
 
---Color Settings--------------------------
+  -- Mason (LSP installer)
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+
+  -- LSP Configuration (simple approach)
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "hrsh7th/cmp-nvim-lsp",
+    },
+    config = function()
+      local lspconfig = require("lspconfig")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      -- Manual LSP setup
+      lspconfig.ruby_lsp.setup({
+        capabilities = capabilities,
+      })
+
+      lspconfig.pyright.setup({
+        capabilities = capabilities,
+        settings = {
+          python = {
+            analysis = {
+              autoSearchPaths = true,
+              useLibraryCodeForTypes = true,
+            }
+          }
+        }
+      })
+
+      lspconfig.terraformls.setup({
+        capabilities = capabilities,
+      })
+
+      -- Key mappings
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = 'Go to definition' })
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = 'Hover documentation' })
+      vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { desc = 'Rename symbol' })
+      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = 'Code actions' })
+      vim.keymap.set('n', 'gr', vim.lsp.buf.references, { desc = 'Find references' })
+    end,
+  },
+
+  -- Autocompletion
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets",
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+
+      -- Load friendly snippets
+      require("luasnip.loaders.from_vscode").lazy_load()
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+          ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+          ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+        }),
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' },
+        }, {
+          { name = 'buffer' },
+          { name = 'path' },
+        })
+      })
+    end,
+  },
+
+  -- Treesitter for syntax highlighting
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = {
+          "ruby",
+          "python", 
+          "hcl",
+          "terraform",
+          "lua",
+          "vim",
+          "vimdoc",
+          "query",
+          "json",
+          "yaml"
+        },
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+        indent = {
+          enable = true,
+        },
+      })
+    end,
+  },
+
+  -- File explorer
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("nvim-tree").setup({
+        view = {
+          width = 30,
+        },
+        renderer = {
+          group_empty = true,
+        },
+        filters = {
+          dotfiles = false,
+        },
+      })
+      vim.keymap.set('n', '<C-n>', ':NvimTreeToggle<CR>', { desc = 'Toggle file tree' })
+    end,
+  },
+
+  -- Telescope fuzzy finder
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.5",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      local telescope = require("telescope")
+      telescope.setup({
+        defaults = {
+          mappings = {
+            i = {
+              ["<C-h>"] = "which_key"
+            }
+          }
+        }
+      })
+
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find files' })
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Live grep' })
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Find buffers' })
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Help tags' })
+    end,
+  },
+
+  -- Status line
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("lualine").setup({
+        options = {
+          theme = "cyberdream",
+        },
+      })
+    end,
+  },
+
+  -- Git integration
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup()
+    end,
+  },
+
+  -- Terraform specific
+  {
+    "hashivim/vim-terraform",
+    ft = "terraform",
+    config = function()
+      vim.g.terraform_align = 1
+      vim.g.terraform_fmt_on_save = 1
+    end,
+  },
+}
+
+-- Setup lazy.nvim
+require("lazy").setup(plugins, {
+  checker = {
+    enabled = true,
+  },
+})
+
+-- Language-specific settings
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "ruby",
+  callback = function()
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.tabstop = 2
+    vim.opt_local.softtabstop = 2
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "python",
+  callback = function()
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.tabstop = 4
+    vim.opt_local.softtabstop = 4
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = {"terraform", "hcl"},
+  callback = function()
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.tabstop = 2
+    vim.opt_local.softtabstop = 2
+  end,
+})
+
+-- Leader key
+vim.g.mapleader = " "
+
+-- Color settings
 vim.cmd('syntax on')
-
-if vim.fn.has('termguicolors') then
-  vim.opt.termguicolors=true
+if vim.fn.has('termguicolors') == 1 then
+  vim.opt.termguicolors = true
 end
---End Color Settings----------------------
+
+-- Key mappings for better navigation
+vim.keymap.set('n', '<leader>w', ':w<CR>', { desc = 'Save file' })
+vim.keymap.set('n', '<leader>q', ':q<CR>', { desc = 'Quit' })
+vim.keymap.set('n', '<Esc>', ':nohlsearch<CR>', { desc = 'Clear search highlight' })
+
+-- Format on save for supported languages
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = {"*.py", "*.rb", "*.tf"},
+  callback = function()
+    vim.lsp.buf.format({ async = false })
+  end,
+})
